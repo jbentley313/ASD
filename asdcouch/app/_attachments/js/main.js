0d1e91f8286cd413beecfad801934b8400bcde4f
Jason Bentley <jbxfade@fullsail.edu>
2012-11-04 19:31:36 -0800

    events still firing twice

--- asdcouch/app/_attachments/js/main.js
+++ asdcouch/app/_attachments/js/main.js
@@ -7,10 +7,11 @@
 //Wait until the DOM is ready
 $(document).on("pageinit", function(){
 
-
+	
 
 // JSON Data Loader
     $('#chicken').on("click", function(){
+    	$('#recipeList').empty();
 	    console.log("Chicken Data!!");
         $.ajax({
             "url": '_view/chicken',
@@ -30,5 +31,102 @@ $(document).on("pageinit", function(){
             }
             
         });
+
     });	
+    
+    $('#beef').on("click", function(){
+    	$('#recipeList').empty();
+	    console.log("Beef Data!!");
+        $.ajax({
+            "url": '_view/beef',
+            "dataType": 'json',
+            "success": function(data){
+
+				$.each(data.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#")
+								.text(RecipeName)
+						)
+					);
+				});
+				$('#recipeList').listview('refresh');
+            }
+            
+        });
+    });
+    
+    $('#pork').on("click", function(){
+    	$('#recipeList').empty();
+	    console.log("Pork Data!!");
+        $.ajax({
+            "url": '_view/pork',
+            "dataType": 'json',
+            "success": function(data){
+
+				$.each(data.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#")
+								.text(RecipeName)
+						)
+					);
+				});
+				$('#recipeList').listview('refresh');
+            }
+            
+        });
+    });
+    
+    $('#veggie').on("click", function(){
+    	$('#recipeList').empty();
+	    console.log("Veggie Data!!");
+        $.ajax({
+            "url": '_view/veggie',
+            "dataType": 'json',
+            "success": function(data){
+
+				$.each(data.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#")
+								.text(RecipeName)
+						)
+					);
+				});
+				$('#recipeList').listview('refresh');
+            }
+            
+        });
+    });
+    
+    $('#date').on("click", function(){
+    	$('#recipeList').empty();
+	    console.log("Date Data!!");
+        $.ajax({
+            "url": '_view/date',
+            "dataType": 'json',
+            "success": function(data){
+
+				$.each(data.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#")
+								.text(RecipeName)
+						)
+					);
+				});
+				$('#recipeList').listview('refresh');
+            }
+            
+        });
+    });
+    
+    
+    
+    
 });
