1084bb40ffa6ceaff98059d297721452dcb9b5f6
Jason Bentley <jbxfade@fullsail.edu>
2012-11-09 17:18:31 -0800

    main.js change to better display json items using JQuery

--- Week4/asdcouch/app/_attachments/js/main.js
+++ Week4/asdcouch/app/_attachments/js/main.js
@@ -43,34 +43,7 @@ $("#home").on("pageinit", function(){
         });
     });	
 	
-//	$('#chickenNav').on("click", function(){
-//	    console.log("Chicken Data!!");
-//        $('#recipeList').empty();
-//        $.ajax({
-//            "url": '_view/chicken',
-//            "type": 'GET',
-//            "dataType": 'json',
-//            "success": function(response){
-//
-//				$.each(response.rows, function(index, recipe) {
-//					$(''+
-//						'<div>' +
-//							'<p>' + recipe.value.RecipeName +'</p>' +
-//							'<p>' + 'Rating: ' + recipe.value.Rating +'</p>' +
-//							'<p>' + 'Meal Time: ' + recipe.value.MealTime +'</p>' +
-//							'<p>' + 'Directions: ' + recipe.value.Directions +'</p>' +
-//							'<hr />' +
-//						'</div>'
-//					).appendTo('#recipeList');
-//					console.log(response);
-//				});
-//            },
-//            error: function(msg) {
-//            	console.log("Error.");
-//            	console.log(msg);
-//            }
-//        });
-//    });
+
     
 	$('#beefNav').on("click", function(){
 	    console.log("Beef Data!!");
@@ -81,18 +54,23 @@ $("#home").on("pageinit", function(){
             "dataType": 'json',
             "success": function(response){
 
-				$.each(response.rows, function(index, recipe) {
-					$(''+
-						'<div>' +
-							'<p>' + recipe.value.RecipeName +'</p>' +
-							'<p>' + 'Rating: ' + recipe.value.Rating +'</p>' +
-							'<p>' + 'Meal Time: ' + recipe.value.MealTime +'</p>' +
-							'<p>' + 'Directions: ' + recipe.value.Directions +'</p>' +
-							'<hr />' +
-						'</div>'
-					).appendTo('#recipeList');
-					console.log(response);
+            	$.each(response.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					var Rating = recipe.value.Rating;
+					var MealTime = recipe.value.MealTime;
+					var Directions = recipe.value.Directions;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#").text(RecipeName).append(
+								$('<p>').text(''),
+								$('<p>').text('Rating: ' + Rating),
+								$('<p>').text('Meal Time: ' + MealTime),
+								$('<p>').text('Directions: ' + Directions)
+							)
+						)
+					);
 				});
+            	$('#recipeList').listview('refresh');
             },
             error: function(msg) {
             	console.log("Error.");
@@ -110,18 +88,23 @@ $("#home").on("pageinit", function(){
             "dataType": 'json',
             "success": function(response){
 
-				$.each(response.rows, function(index, recipe) {
-					$(''+
-						'<div>' +
-							'<p>' + recipe.value.RecipeName +'</p>' +
-							'<p>' + 'Rating: ' + recipe.value.Rating +'</p>' +
-							'<p>' + 'Meal Time: ' + recipe.value.MealTime +'</p>' +
-							'<p>' + 'Directions: ' + recipe.value.Directions +'</p>' +
-							'<hr />' +
-						'</div>'
-					).appendTo('#recipeList');
-					console.log(response);
+            	$.each(response.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					var Rating = recipe.value.Rating;
+					var MealTime = recipe.value.MealTime;
+					var Directions = recipe.value.Directions;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#").text(RecipeName).append(
+								$('<p>').text(''),
+								$('<p>').text('Rating: ' + Rating),
+								$('<p>').text('Meal Time: ' + MealTime),
+								$('<p>').text('Directions: ' + Directions)
+							)
+						)
+					);
 				});
+            	$('#recipeList').listview('refresh');
             },
             error: function(msg) {
             	console.log("Error.");
@@ -139,18 +122,23 @@ $("#home").on("pageinit", function(){
             "dataType": 'json',
             "success": function(response){
 
-				$.each(response.rows, function(index, recipe) {
-					$(''+
-						'<div>' +
-							'<span>' + recipe.value.RecipeName +'</span>' +
-							'<p>' + 'Rating: ' + recipe.value.Rating +'</p>' +
-							'<p>' + 'Meal Time: ' + recipe.value.MealTime +'</p>' +
-							'<p>' + 'Directions: ' + recipe.value.Directions +'</p>' +
-							'<hr />' +
-						'</div>'
-					).appendTo('#recipeList');
-					console.log(response);
+            	$.each(response.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					var Rating = recipe.value.Rating;
+					var MealTime = recipe.value.MealTime;
+					var Directions = recipe.value.Directions;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#").text(RecipeName).append(
+								$('<p>').text(''),
+								$('<p>').text('Rating: ' + Rating),
+								$('<p>').text('Meal Time: ' + MealTime),
+								$('<p>').text('Directions: ' + Directions)
+							)
+						)
+					);
 				});
+            	$('#recipeList').listview('refresh');
             },
             error: function(msg) {
             	console.log("Error.");
@@ -168,18 +156,24 @@ $("#home").on("pageinit", function(){
             "dataType": 'json',
             "success": function(response){
 
-				$.each(response.rows, function(index, recipe) {
-					$(''+
-						'<div>' +
-							'<p>' + recipe.value.RecipeName +'</p>' +
-							'<p>' + 'Rating: ' + recipe.value.Rating +'</p>' +
-							'<p>' + 'Meal Time: ' + recipe.value.MealTime +'</p>' +
-							'<p>' + 'Directions: ' + recipe.value.Directions +'</p>' +
-							'<hr />' +
-						'</div>'
-					).appendTo('#recipeList');
-					console.log(response);
+            	$.each(response.rows, function(index, recipe) {
+					var RecipeName = recipe.value.RecipeName;
+					var Rating = recipe.value.Rating;
+					var MealTime = recipe.value.MealTime;
+					var Directions = recipe.value.Directions;
+					$('#recipeList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "#").text(RecipeName).append(
+								$('<p>').text(''),
+								$('<p>').text('Rating: ' + Rating),
+								$('<p>').text('Meal Time: ' + MealTime),
+								$('<p>').text('Directions: ' + Directions)
+							)
+						)
+					);
 				});
+            	$('#recipeList').listview('refresh');
+            	
             },
             error: function(msg) {
             	console.log("Error.");
